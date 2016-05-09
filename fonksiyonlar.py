3783348ef315c3d5b7c2874b7070ee14c966a45a
ibrahimirdem <irdem007@gmail.com>
2016-05-10 02:22:39 +0300

    Update fonksiyonlar.py

--- fonksiyonlar.py
+++ fonksiyonlar.py
@@ -22,8 +22,8 @@ def numara_denetim(numara):
     browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US)     AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
     browser.open("http://m.facebook.com/")
     browser.select_form(nr=0)
-    browser.form['email'] = 'irdem1'
-    browser.form['pass'] = '05357007448777'
+    browser.form['email'] = 'Facebook Kullanıcı Adı'
+    browser.form['pass'] = 'Facebook Şifre'
     response = browser.submit()
 
 
